55c216e8d5f2946d5e14657edef9a6d259c8c8af
cy123-coder <wolgugu@gmail.com>
2026-08-07 00:54:08 +0900

    Feat: 홈, 헤더, 네비게이션바, 모바일 레이아웃 작업

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../styles/Header.css'
+
+const Header = () => {
+    return(
+        <>
+        <div className = "wrapper">
+        <div className="headerbox">로고</div></div>
+        </>
+    );
+}
+
+export default Header;
