4840ea3bc49688777e92f303080b847a4ce22f8b
cy123-coder <wolgugu@gmail.com>
2026-08-19 21:20:54 +0900

    임시저장

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/Signup.css";
+import "../styles/SignUp.css";
 
 export default function Signup() {
   const navigate = useNavigate();
